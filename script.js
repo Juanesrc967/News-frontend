bda6c772aa96a7338484de81cb99f4564296f942
Juan Rojas <juan2005rc@gmail.com>
2025-01-05 20:41:14 -0500

    Actualizacion 3

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ btnTimes.addEventListener('click', function(){
         navBar.classList.remove("show-nav");
     }
 })
+
+//Seguridad 
+// Deshabilitar clic derecho
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+// Deshabilitar F12 (Herramientas de desarrollo)
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }else if (e.ctrlKey && e.shiftKey && e.key === 'I') { 
+        e.preventDefault();
+    } else if (e.ctrlKey && e.shiftKey && e.key === 'J') { 
+        e.preventDefault();
+    }else if (e.ctrlKey && e.key === 'U') { 
+        e.preventDefault();
+    }
+});
